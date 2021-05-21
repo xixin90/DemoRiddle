a064bbfba4063489a569e826a978bd5b2a5d03b3
Liew Xi Xin <20019652@myrp.edu.sg>
2021-05-21 09:41:49 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20019652/demoriddle/AnswerActivity1.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20019652.demoriddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnswerActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer1);
+    }
+}
